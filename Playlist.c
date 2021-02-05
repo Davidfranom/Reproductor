25ddf0ed7105d3c0ef91027d92c253ac37d66a35
dvgr1 <77249207+dvgr1@users.noreply.github.com>
2021-02-05 05:52:26 +0100

    Playlist.c y .h documentados con doxygen
    Listos para compilar en un doxyfile con los demas archivos documentados

--- Playlist.c
+++ Playlist.c
@@ -1,23 +1,45 @@
 #include "Playlist.h"
-#include<string.h>
+#include <string.h>
 
-
+/**
+ * @brief  Crea un nuevo nodo e inicializa sus valores
+ *
+ * @param Track* x  Referencia a objeto Track
+ *
+ * @return n Dirección de memoria del objeto nodo
+ *
+ * @pre Debe haber espacio de memoria dinamica disponible
+ *
+ * @post La asignación de memoria fue exitosa
+ */
 static Node* newNode( Track* x )
 {
 	Node* n = (Node*)malloc(sizeof(Node));
 	if(n){
-		n->datos = *x; //  Verificar si funciona
+		n->datos = *x;
 		n->next = NULL;
 		n->prev = NULL;
 	}
 	return n;
 }
 
-Playlist* Playlist_New(char name[])
+/**
+ * @brief Crea un nuevo objeto tipo Playlist
+ *
+ * @param name[] Arreglo de caracteres que se copía al campo name
+ *
+ * @return lista Dirección de memoria del objeto Playlist
+ *
+ * @pre Debe haber espacio de memoria dinamica disponible
+ *
+ * @post La asignación de memoria fue exitosa
+ */
+
+Playlist* Playlist_New( char name[] )
 {
 	Playlist* lista = (Playlist*)malloc(sizeof( Playlist ));
-	if(lista){
-		strcpy(lista->name,name);
+	if( lista ){
+		strcpy( lista->name, name );
 		lista->cursor = NULL;
 		lista->first = NULL;
 		lista->last = NULL;
@@ -28,21 +50,47 @@ Playlist* Playlist_New(char name[])
 	return lista;
 }
 
+
+/**
+ * @brief Devuelve la memoria pedida por un objeto Playlist
+ *
+ * @param Playlist** this Doble apuntador al objeto a borrar
+ *
+ * @pre El objeto Playlist debe existir
+ *      No puede ser borrado dos veces
+ *
+ * @post Se regreso la memoria y no hubo fugas
+ *
+ */
+
 void Playlist_Delete( Playlist** this )
 {
-	size_t c=(*this)->len;
-	for(size_t i = 0; i < c ;++i){
+	size_t c = (*this)->len;
+	for(size_t i = 0; i < c ; ++i ){
 		Node* tmp = (*this)->first->next;
-		free((*this)->first);
+		free( (*this)->first );
 		(*this)->first = tmp;
 	}
 	free(*this);
 	*this=NULL;
 }
 
+
+/**
+ * @brief Inserta una cancion (Track) en la lista (Playlist)
+ *
+ * @param lista Una referencia a la playlist (lista) donde se insertara la canción
+ * @param cancion  Referencia a la canción que se insertara
+ *
+ * @pre La playlist ya debe existir (estar creada)
+ *      La canción debe existir (estar creada)
+ *
+ * @post La canción se inserto en la lista sin problemas
+ */
+
 void Playlist_Insert( Playlist* this, Track* x )
 {
-	Node* n= newNode(x);
+	Node* n = newNode(x);
 	if( Playlist_IsEmpty(this) ){
 		this->first = this->last = this->cursor = n;
 		this->last->next = this->first;
@@ -62,10 +110,24 @@ void Playlist_Insert( Playlist* this, Track* x )
 	++this->len;
 }
 
+
+/**
+ * @brief Inseta una canción en el frente de la lista
+ *
+ * @param lista Referencia a la lista donde se insertara la canción
+ * @param canción Referencia a la canción que se insertara
+ *
+ * @pre La lista debe existir
+ *      La canción debe existir
+ *
+ * @post La canción se inserto al frente de la lista; donde apunta
+ *       first
+ */
+
 void Playlist_Insert_front( Playlist* this, Track* x )
 {
-	Node* n= newNode(x);
-	if(Playlist_IsEmpty(this)){
+	Node* n = newNode(x);
+	if( Playlist_IsEmpty(this) ){
 		this->first = this->last = this->cursor = n;
 	} else{
 		n->next = this->first;
@@ -79,10 +141,24 @@ void Playlist_Insert_front( Playlist* this, Track* x )
 
 }
 
+
+/**
+ * @brief Inserta una canción al final de la lista
+ *
+ * @param lista Referencia a la lista donde se insertara la canción
+ * @param canción Referencia a la canción a insertar
+ *
+ * @pre La lista debe existir
+ *      La canción debe existir
+ *
+ * @post La canción se inserto al final de la lista, donde apunta last
+ *
+ */
+
 void Playlist_Insert_back( Playlist* this, Track* x )
 {
 	Node* n = newNode(x);
-	if(Playlist_IsEmpty(this)){
+	if( Playlist_IsEmpty(this) ){
 		this->first = this->last = this->cursor = n;
 	} else{
 		this->last->next = n;
@@ -96,66 +172,121 @@ void Playlist_Insert_back( Playlist* this, Track* x )
 
 }
 
+
+/**
+ * @brief Elimina el elemento de la lista apuntado por el cursor
+ *
+ * @param lista Referencia a la lista donde esta la canción
+ *                       que sera removida
+ *
+ * @pre La lista debe existir
+ *      La lista debe tener como mínimo un elemento
+ *      El cursor debe apuntar a una dirección de memoria valida
+ *
+ * @post La lista debera tener un nodo menos
+ *       El cursor se movera a la derecha, es decir al siguiente nodo
+ *
+ */
+
 void Playlist_Remove( Playlist* this )
 {
 	assert( this->len > 0 );
 
 	if( this->len == 1 ){
-		free(this->first);
+		free( this->first );
 		this->first = this->last = this->cursor = NULL;
 		--this->len;
-	} else if(this->cursor == this->first){
-		Playlist_Remove_front(this);
+	} else if( this->cursor == this->first ){
+		Playlist_Remove_front( this );
 		this->cursor = this->first;
-	} else if(this->cursor == this->last ){
-		Playlist_Remove_back(this);
+	} else if( this->cursor == this->last ){
+		Playlist_Remove_back( this );
 		this->cursor = this->last;
 	} else{
 		Node* tmp1 = this->cursor->prev;
 		Node* tmp2 = this->cursor->next;
-		free(this->cursor);
-		tmp1->next=tmp2;
-		tmp2->prev=tmp1;
-		this->cursor=tmp2;
+		free( this->cursor );
+		tmp1->next = tmp2;
+		tmp2->prev = tmp1;
+		this->cursor = tmp2;
 		--this->len;
 	}
 }
 
+/**
+ * @brief Elimina el elemento que se encuentra en el frente de la lista
+ *
+ * @param lista Referencia a la lista de donde se removera la canción
+ *
+ * @pre La lista debe existir
+ *      La lista debe tener como mínimo un elemento
+ *
+ * @post La lista debera tener un nodo menos
+ *
+ */
 void Playlist_Remove_front( Playlist* this )
 {
-	assert(this->len > 0);
+	assert( this->len > 0 );
 
 	if( this->len == 1 ){
-		free(this->first);
+		free( this->first );
 		this->first = this->last = this->cursor =  NULL;
 	} else{
-		Node* tmp=this->first->next;
-		free(this->first);
-		this->first=tmp;
-		tmp->prev=this->last;
-		this->last->next=tmp;
+		Node* tmp = this->first->next;
+		free( this->first );
+		this->first = tmp;
+		tmp->prev = this->last;
+		this->last->next = tmp;
 	}
 	--this->len;
 }
 
+
+/**
+ * @brief Elimina el elemento que se encuentra al final de la lista
+ *
+ * @param lista Referencia a la lista de donde se removera la canción
+ *
+ * @pre La lista debe existir
+ *      La lista debe tener como mínimo un elemento
+ *
+ * @post La lista debera tener un nodo menos
+ *
+ */
 void Playlist_Remove_back( Playlist* this )
 {
-	assert(this->len > 0);
+	assert( this->len > 0 );
 
-	if(this->len == 1){
-		free(this->last);
+	if( this->len == 1 ){
+		free( this->last );
 		this->first = this->last = this->cursor = NULL;
 	}else{
 		Node* tmp = this->last->prev;
-		free(this->last);
-		this->last=tmp;
-		tmp->next=this->first;
-		this->first->prev=tmp;
+		free( this->last );
+		this->last = tmp;
+		tmp->next = this->first;
+		this->first->prev = tmp;
 	}
 	--this->len;
 }
 
 
+
+/**
+ * @brief Devuelve una canción. Otorga la dirección de un objeto canción
+          apuntado por el cursor y almacenado en una lista tipo playlist
+ *
+ * @param lista Referencia a la playlist que contiene la canción
+ *
+ * @return dirección del objeto canción
+ *
+ * @pre La lista debe existir
+ *      Debe haber por lo menos una canción almacenada en la lista
+ *      El cursor debe apuntar a una dirección de memoria valida
+ *
+ * @post La dirección regresada pertenece a un objeto canción
+ *
+ */
 Track* Playlist_Get( Playlist* this ) // se tiene que modificar
 {
 	assert( this->cursor );
@@ -163,40 +294,125 @@ Track* Playlist_Get( Playlist* this ) // se tiene que modificar
 }
 
 
+
+
+/**
+ * @brief Posiciona al cursor al frente de la lista
+ *
+ * @param lista Referencia a la playlist donde nos moveremos
+ *
+ * @pre La Lista debe existir
+ *      El cursor debe apuntar a una dirección de memoria valida
+ *
+ * @post La lista no se modifica, unicamente es recorrida por el cursor
+ *       El cursor apunta al frente de la lista
+ */
 void Playlist_Cursor_front( Playlist* this )
 {
     assert( this->cursor );
 	this->cursor = this->first;
 }
 
+
+/**
+ * @brief Posiciona al cursor al final de la lista
+ *
+ * @param lista Referencia a la playlist donde nos moveremos
+ *
+ * @pre La lista debe existir
+ *      El cursor debe apuntar a una dirección de memoria valida
+ *
+ * @post La lista no se modifica, unicamente es recorrida por el cursor
+ *       El cursor apunta a la parte trasera de la lista
+ */
 void Playlist_Cursor_back( Playlist* this )
 {
     assert( this->cursor );
 	this->cursor = this->last;
 }
 
+
+/**
+ * @brief Mueve el cursor al siguiente nodo
+ *
+ * @param lista Referencia a la playlist donde nos moveremos
+ *
+ * @pre La lista debe existir
+ *      El cursor debe apuntar a una dirección de memoria valida
+ *
+ * @post El cursor apunta al nodo siguiente al actual
+ */
 void Playlist_Cursor_next( Playlist* this )
 {
 	assert(this->cursor);
 	this->cursor=this->cursor->next;
 }
 
+
+
+/**
+ * @brief Mueve el cursor al nodo anterior al actual
+ *
+ * @param lista Referencia a la playlist donde nos moveremos
+ *
+ * @pre La lista debe existir
+ *      El cursor debe apuntar a una dirección de memoria valida
+ *
+ * @post El cursor apunta al nodo predecesor del nodo actual
+ *
+ */
 void Playlist_Cursor_prev( Playlist* this )
 {
 	assert(this->cursor);
 	this->cursor=this->cursor->prev;
 }
 
+
+/**
+ * @brief Indica si la lista esta vacía
+ *
+ * @param lista Referencia a la lista de la cual se quiere conocer su estado
+ *
+ * @return true si la lista esta vacia, len = 0, false si
+ *         la lista esta tiene mínimo un elemento, len != 0
+ *
+ * @pre La lista debe existir
+ *
+ */
 bool Playlist_IsEmpty( Playlist* this )
 {
 	return this->len == 0;
 }
 
+
+/**
+ * @brief Da el número de elementos en la lista
+ *
+ * @param lista Referencia a la lista de la que se quiere conocer su estado
+ *
+ * @return len que es el número de elementos en la lista
+ *
+ * @pre La lista debe existir
+ */
 size_t Playlist_Len( Playlist* this )
 {
 	return this->len;
 }
 
+
+/**
+ * @brief Limpia la lista. Elimina todos los nodos de la lista hasta el momento,
+ *        len seria igual a cero y cursor, first y last, apuntan a NULL
+ *
+ * @param lista Referencia a la lista a limpiar
+ *
+ * @pre La lista debe existir
+ *      len != 0 por lo tanto la lista debe tener al menos un elemento
+ *
+ * @post La lista no contendra a ningun nodo
+ *       los apuntadores first, last y cursor apuntan a NULL
+ *
+ */
 void Playlist_MakeEmpty( Playlist* this )
 {
 	size_t c=this->len;
@@ -206,6 +422,19 @@ void Playlist_MakeEmpty( Playlist* this )
 }
 
 
+
+
+/**
+ * @brief  Recorre la lista y hace algo en cada uno de sus elementos
+ *
+ * @param lista Referencia a la lista de trabajo
+ * @param fn Esta función hace algo con el campo de datos
+ *
+ * @pre La lista debe existir
+ *      fn debe estar definida y escrita
+ *      Los argumentos de fn deben ser del mismo tipo y deben existir
+ *
+ */
 void Playlist_Traverse( Playlist* this, void (*fn)( Track item,size_t c ) ) // se tiene que modificar
 {
 	if( NULL == this ){ return; }
@@ -222,7 +451,18 @@ void Playlist_Traverse( Playlist* this, void (*fn)( Track item,size_t c ) ) // s
 
 }
 
-void Playlist_GralRemove( Playlist* this, size_t id ) 
+
+/**
+ * @brief Busca un elemento en la lista por su ID y lo elimina
+ *
+ * @param lista Referencia a lista de trabajo
+ * @param id Entero que sirve como identificador unico de cada canción
+ *
+ * @pre La lista debe existir
+ *      La lista debe contener al menos una canción
+ *
+ */
+void Playlist_GralRemove( Playlist* this, size_t id )
 {
 	if( NULL == this ){ return; }
 	Node* t = this->first;
@@ -238,16 +478,51 @@ void Playlist_GralRemove( Playlist* this, size_t id )
    }
 }
 
+
+/**
+ * @brief Devuelve el ID de una canción apuntada por el cursor
+ *
+ * @param lista Referencia a la lista de trabajo
+ *
+ * @return id de una canción
+ *
+ * @pre La lista debe existir
+ *      La lista debe contener canciones, no estar vacía
+ *      El cursor debe apuntar a una dirección de memoria valida
+ *
+ */
 size_t Playlist_GetID(Playlist* this){
 	assert(this->cursor);
 	return this->cursor->datos.id;
 }
 
+
+/**
+ * @brief Devuelve el nombre de la lista
+ *
+ * @param lista Referencia a la lista de trabajo
+ *
+ * @return el nombre de la playlist
+ *
+ * @pre La lista debe existir
+ *
+ */
 char* Playlist_GetName(Playlist* this){
 	assert(this);
 	return (char*)this->name;
 }
 
+
+/**
+ * @brief Muestra en pantalla el nombre de la playlist
+ *
+ * @param lista Copia de la lista de trabajo
+ * @param c Numero de la lista
+ *
+ * @pre La lista debe existir
+ *      Debe existir al menos una lista
+ *
+ */
 //Para Traverse
 void Print_PlaylistName(Playlist item,size_t c){
 	printf("%ld. %s\n",c,item.name);
--- Playlist.h
+++ Playlist.h
@@ -1,11 +1,36 @@
+/**
+ * @file Playlist.h
+ *
+ * @brief API para el TAD Playlist
+ *
+ *   Este archivo contiene la API del nuestro tipo abstracto de nivel superior
+ *   llamado Playlist el cual sirve como un contenedor de canciones enlazadas
+ *   una con otra; estan todas las funciones con las cuales trabaja este TAD
+ *
+ * @author Equipo 1 EDA 1
+ *
+ * @date 04/02/2021
+ *
+ */
+
 #ifndef  PLAYLIST_INC
 #define  PLAYLIST_INC
 
+
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <assert.h>
 #include "Track.h"
+
+
+/**
+ * Esta estructura Node representa a un nodo de una lista
+ * circular doblemente enlazada, el campo Track datos es
+ * tiene como proposito almacenar una canción que esta
+ * representada por la estructura Track, y los apuntadores
+ * next y prev ayudan a conectar un nodo con otro y asi generar
+ * a la playlist
+ */
 
 typedef struct Node{
 
@@ -14,9 +39,21 @@ typedef struct Node{
 	struct Node* prev;
 } Node;
 
-typedef struct Playlist{
-    char name[MAX]; //Nombre del playlist, en prueba
-    
+
+/**
+ * La estructura Playlist representa a una lista circular
+ * doblemente enlazada, la cual funcionara como una Playlist
+ * teniendo apuntadores a nodos, los cuales contienen las canciones
+ * el campo name funciona como contenedor para el nombre de la
+ * playlist, los apuntadores first, last y cursor sirven para
+ * movernos por medio de la playlist, saber donde empieza y donde
+ * acaba y el campo len sirve para llevar el conteo de cuantos
+ * nodos han sido ingresados a la lista
+ */
+
+typedef struct Playlist
+{
+    char name[MAX];
 	Node* first;
 	Node* last;
 	Node* cursor;
@@ -24,34 +61,27 @@ typedef struct Playlist{
 
 } Playlist;
 
-Playlist* Playlist_New(char name[]);
-void Playlist_Delete( Playlist** this );
 
-void Playlist_Insert_front( Playlist* this, Track* x );
-void Playlist_Insert_back( Playlist* this, Track* x );
-void Playlist_Insert( Playlist* this, Track* x );
-
-void Playlist_Remove_front( Playlist* this );
-void Playlist_Remove_back( Playlist* this );
-void Playlist_Remove( Playlist* this );
-
-Track* Playlist_Get( Playlist* this );
-
-void Playlist_Cursor_front( Playlist* this );
-void Playlist_Cursor_back( Playlist* this );
-void Playlist_Cursor_next( Playlist* this );
-void Playlist_Cursor_prev( Playlist* this );
-
-bool Playlist_IsEmpty( Playlist* this );
-size_t Playlist_Len( Playlist* this );
-
-void Playlist_MakeEmpty( Playlist* this );
-void Playlist_Traverse( Playlist* this, void (*fn)( Track item,size_t c  ) );
-void Playlist_GralRemove( Playlist* this, size_t id );
-size_t Playlist_GetID(Playlist* this);
-
-char* Playlist_GetName(Playlist* this);
-
-void Print_PlaylistName(Playlist item,size_t c); 
+Playlist* Playlist_New(          char name[]);
+void      Playlist_Delete(       Playlist** this );
+void      Playlist_Insert_front( Playlist* this, Track* x );
+void      Playlist_Insert_back(  Playlist* this, Track* x );
+void      Playlist_Insert(       Playlist* this, Track* x );
+void      Playlist_Remove_front( Playlist* this );
+void      Playlist_Remove_back(  Playlist* this );
+void      Playlist_Remove(       Playlist* this );
+Track*    Playlist_Get(          Playlist* this );
+void      Playlist_Cursor_front( Playlist* this );
+void      Playlist_Cursor_back(  Playlist* this );
+void      Playlist_Cursor_next(  Playlist* this );
+void      Playlist_Cursor_prev(  Playlist* this );
+bool      Playlist_IsEmpty(      Playlist* this );
+size_t    Playlist_Len(          Playlist* this );
+void      Playlist_MakeEmpty(    Playlist* this );
+void      Playlist_Traverse(     Playlist* this, void (*fn)( Track item,size_t c  ) );
+void      Playlist_GralRemove(   Playlist* this, size_t id );
+size_t    Playlist_GetID(        Playlist* this);
+char*     Playlist_GetName(      Playlist* this);
+void      Print_PlaylistName(    Playlist item, size_t c);
 
 #endif
